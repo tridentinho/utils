d27fa464b65f6077b8efca751d99609cdb4f4037
Lucas Gulart <lucas.silveira@compasso.com.br>
2021-06-11 16:04:56 -0300

    Initial commit

---
+++ socket_client/client.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+API_BASE = process.env.API_BASE;
+TOKEN = process.env.TOKEN;
+NAMESPACE = process.env.NAMESPACE;
+
+const socket = require('socket.io-client')(`${API_BASE}${NAMESPACE}`, {extraHeaders: {
+    authorization: TOKEN
+  }});
+const someDelay = 10;
+socket.on('connect', function () {
+    console.log('connected...');
+    if (process.argv[2] && process.argv[3]) {
+        console.log('sending ' + process.argv[2] + ': ' + process.argv[3]);
+        socket.emit(process.argv[2], process.argv[3]);
+        setTimeout(() => {
+            process.exit(0);  
+        }, someDelay);
+    } else {
+        console.log('usage: ./client.js <event> <data>');
+        process.exit(1);
+    }
+});
